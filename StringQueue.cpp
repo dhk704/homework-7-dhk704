1d6c25c17a91186fc4f823bff76b13f7adbb1518
David <dhk704@gmail.com>
2025-04-30 17:23:20 -0700

    Set up header file & start StringQueue.cpp

--- StringQueue.cpp
+++ StringQueue.cpp
@@ -1,5 +1,42 @@
-//
-// Created by David on 4/30/2025.
-//
-
 #include "StringQueue.h"
+#include <iostream>
+
+// Default constructor
+StringQueue::StringQueue()
+    : m_data(nullptr), m_dataSize{8} {
+    std::cout << "Default construct queue" << std::endl;
+}
+
+// Destructor
+StringQueue::~StringQueue() {
+    std::cout << "Destruct queue" << std::endl;
+    delete[] m_data;
+}
+
+
+size_t StringQueue::size() const {
+    return m_count;
+}
+
+size_t StringQueue::capacity() const {
+    return m_dataSize;
+}
+
+void StringQueue::clear() {
+    m_count = 0;
+    m_front = 0;
+    m_rear = 0;
+}
+
+std::string StringQueue::dequeue() {
+    // NEEDS WORK: Move element at front of queue to temp string, increment m_front if end is reached
+    if (m_count > 0) {
+        --m_count;
+        // Return temp string
+        return m_data[m_count];
+    }
+    else {
+        throw std::runtime_error("Can't dequeue an empty queue");
+    }
+}
+
--- StringQueue.h
+++ StringQueue.h
@@ -1,14 +1,43 @@
-//
-// Created by David on 4/30/2025.
-//
-
 #ifndef STRINGQUEUE_H
 #define STRINGQUEUE_H
-
-
+#include <string>
 
 class StringQueue {
+    // An array in dynamic memory that stores the elements of the queue.
+    std::string* m_data;
+    // The length of the currently-used m_data array, initially 8.
+    size_t m_dataSize;
+    // The number of elements that are still in the queue, initially 0
+    size_t m_count = {0};
+    // The index of where the "front" element is stored, initially 0.
+    size_t m_front = {0};
+    // The index of where the next element to be enqueued will be placed, initially 0.
+    size_t m_rear = {0};
 
+public:
+    StringQueue();
+    // Destructor that frees the m_data array
+    ~StringQueue();
+    // Copy constructor that performs a deep copy of its parameter
+    StringQueue(const StringQueue& other);
+
+    StringQueue(StringQueue&& other);
+
+    StringQueue& operator=(const StringQueue& rhs);
+    StringQueue& operator=(StringQueue&& rhs);
+
+    // Returns the number of elements in the queue
+    size_t size() const;
+    // Returns the number of elements that could be added to the queue without requiring it to resize.
+    size_t capacity() const;
+
+    void clear();
+    // First resizes the queue if its capacity is 0; and then moves the value into m_data at the index specified by m_rear. m_rear is incremented, resetting back to 0 if it has reached the end of the data array
+    void enqueue(std::string value);
+    // Moves the element at the front of the queue into a temporary string, increments m_front (wrapping back to 0 if necessary), and returns the temporary
+    std::string dequeue();
+
+    friend std::ostream& operator<<(std::ostream& lhs, const StringQueue& rhs);
 };
 
 
